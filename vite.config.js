c624414bb746aa19f2807e36f745670937a456cd
Robirider <robidb1@gmail.com>
2026-03-30 12:02:03 +0200

    Actualización: Cambios en archivos y carpetas

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/rally-robibook-v2/' // Cambia esto si el repo se llama distinto
+  base: '/rally-robibook-pro/',
 })
