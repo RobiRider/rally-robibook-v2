b0e8c11fd372c87e623f9e337e30705e1c30c25b
Robirider <robidb1@gmail.com>
2026-03-30 12:18:50 +0200

    Corregir ruta base a v2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/rally-robibook-pro/',
+  // Cambia lo que haya aquí por el nombre exacto de tu nuevo repositorio
+  base: '/rally-robibook-v2/' 
 })
